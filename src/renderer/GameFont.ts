afaa8cfc7d8ee1c120b78447e29569b11376ea74
platz1de <51201131+platz1de@users.noreply.github.com>
2025-05-21 22:51:37 +0200

    Cleanup font logic

--- src/renderer/GameFont.ts
+++ src/renderer/GameFont.ts
@@ -9,15 +9,13 @@ export class GameFont {
 	private readonly vao: WebGLVertexArrayObject;
 	private readonly uniforms: WebGLUniforms<"texture_data" | "offset" | "size">;
 	private readonly texture: WebGLTexture;
-	private readonly charData: { x: number, y: number, xOffset: number, yOffset: number, xAdvance: number, width: number, height: number }[] = [];
-	private readonly image: HTMLImageElement;
+	private readonly charData: { sx1: number, sy1: number, sx2: number, sy2: number, tx1: number, ty1: number, tx2: number, ty2: number, xAdvance: number }[] = [];
 	private readonly positionBuffer: WebGLBuffer;
 	private readonly charBuffer: WebGLBuffer;
 	private readonly blurBuffer: WebGLBuffer;
 	private readonly lineHeight: number;
 
 	constructor(context: GameGLContext, image: HTMLImageElement, data: Uint8Array) {
-		this.image = image;
 		this.context = context;
 		this.program = context.requireProgram(msdfTextureVertexShader, msdfTextureFragmentShader, "Font renderer failed to init");
 		this.positionBuffer = context.createBuffer();
@@ -26,7 +24,7 @@ export class GameFont {
 		this.vao = context.createVertexArray(this.program, {name: "pos", size: 2, type: WebGL2RenderingContext.FLOAT, buffer: this.positionBuffer}, {name: "char", size: 2, type: WebGL2RenderingContext.FLOAT, buffer: this.charBuffer}, {name: "blur", size: 1, type: WebGL2RenderingContext.FLOAT, buffer: this.blurBuffer});
 		this.uniforms = context.loadUniforms(this.program, "texture_data", "offset", "size");
 		this.texture = context.createTexture(image.width, image.height, image, {minFilter: WebGL2RenderingContext.LINEAR, magFilter: WebGL2RenderingContext.LINEAR});
-		this.lineHeight = this.parseData(data);
+		this.lineHeight = this.parseData(data, image.width, image.height);
 	}
 
 	static async fromRaw(context: GameGLContext, image: string, data: string): Promise<GameFont> {
@@ -34,13 +32,12 @@ export class GameFont {
 			const img = new Image();
 			img.src = image;
 			img.onload = (() => {
-				console.log(data.length)
 				resolve(new GameFont(context, img, Uint8Array.from(atob(data), c => c.charCodeAt(0))));
 			});
 		});
 	}
 
-	private parseData(data: Uint8Array): number {
+	private parseData(data: Uint8Array, textureWidth: number, textureHeight: number): number {
 		const reader = new StreamReader(data);
 		const charCount = reader.readBits(16);
 		for (let i = 0; i < charCount; i++) {
@@ -52,17 +49,20 @@ export class GameFont {
 			const xAdvance = reader.readBits(8);
 			const width = reader.readBits(8);
 			const height = reader.readBits(8);
-			this.charData[char] = {x, y, xOffset, yOffset, xAdvance, width, height};
+			this.charData[char] = {
+				sx1: x / textureWidth, sy1: y / textureHeight, sx2: (x + width) / textureWidth, sy2: (y + height) / textureHeight,
+				tx1: xOffset, ty1: yOffset, tx2: xOffset + width, ty2: yOffset + height,
+				xAdvance
+			};
 		}
 		return reader.readBits(8);
 	}
 
-	drawText(data: { string: string, nameX: number, nameY: number, size: number, baselineBottom?: boolean }[]) {
+	drawText(data: GameTextEntry[]) {
 		this.context.bind(this.program, this.vao);
 
 		this.context.bindTexture(this.texture, 0);
 
-		//TODO: calculate factor based on character width
 		const parentWidth = this.context.raw.canvas.width;
 		const parentHeight = this.context.raw.canvas.height;
 
@@ -73,50 +73,47 @@ export class GameFont {
 		const blur = new Float32Array(characterCount * 6);
 
 		let offset = 0;
-		for (const text of data) {
-			let {string, nameX, nameY, size, baselineBottom} = text;
+		for (const entry of data) {
 			let xOffset = 0;
 			let length: number = 0;
-			for (let i = 0; i < string.length; i++) {
-				const charData = this.charData[string[i].charCodeAt(0)]
-				if (!charData) throw new Error(`Char ${string[i]} not found in font data`);
+			for (let i = 0; i < entry.string.length; i++) {
+				const charData = this.charData[entry.string.charCodeAt(i)]
+				if (!charData) throw new Error(`Char ${entry.string[i]} not found in font data`);
 				length += charData.xAdvance;
 			}
-			const fontSize = Math.min(size / length, 0.4 * size / this.lineHeight);
-			nameX += (size - fontSize * length) / 2;
-			if (baselineBottom) nameY += size / 2 - fontSize * this.lineHeight;
-			for (let i = offset; i < offset + string.length; i++) {
-				const charData = this.charData[string.charCodeAt(i - offset)];
-				if (!charData) throw new Error(`Char ${string[i - offset]} not found in font data`);
-
-				positions[12 * i] = nameX + (xOffset + charData.xOffset) * fontSize;
-				positions[12 * i + 1] = nameY + charData.yOffset * fontSize;
-				positions[12 * i + 2] = nameX + (xOffset + charData.xOffset + charData.width) * fontSize;
-				positions[12 * i + 3] = nameY + charData.yOffset * fontSize;
-				positions[12 * i + 4] = nameX + (xOffset + charData.xOffset + charData.width) * fontSize;
-				positions[12 * i + 5] = nameY + (charData.yOffset + charData.height) * fontSize;
-				positions[12 * i + 6] = nameX + (xOffset + charData.xOffset) * fontSize;
-				positions[12 * i + 7] = nameY + charData.yOffset * fontSize;
-				positions[12 * i + 8] = nameX + (xOffset + charData.xOffset) * fontSize;
-				positions[12 * i + 9] = nameY + (charData.yOffset + charData.height) * fontSize;
-				positions[12 * i + 10] = nameX + (xOffset + charData.xOffset + charData.width) * fontSize;
-				positions[12 * i + 11] = nameY + (charData.yOffset + charData.height) * fontSize;
-				characters[12 * i] = charData.x / this.image.width;
-				characters[12 * i + 1] = charData.y / this.image.height;
-				characters[12 * i + 2] = (charData.x + charData.width) / this.image.width;
-				characters[12 * i + 3] = charData.y / this.image.height;
-				characters[12 * i + 4] = (charData.x + charData.width) / this.image.width;
-				characters[12 * i + 5] = (charData.y + charData.height) / this.image.height;
-				characters[12 * i + 6] = charData.x / this.image.width;
-				characters[12 * i + 7] = charData.y / this.image.height;
-				characters[12 * i + 8] = charData.x / this.image.width;
-				characters[12 * i + 9] = (charData.y + charData.height) / this.image.height;
-				characters[12 * i + 10] = (charData.x + charData.width) / this.image.width;
-				characters[12 * i + 11] = (charData.y + charData.height) / this.image.height;
+			const fontSize = Math.min(entry.size / length, 0.4 * entry.size / this.lineHeight);
+			const x = entry.x + (entry.size - fontSize * length) / 2;
+			const y = entry.baselineBottom ? entry.y + entry.size / 2 - fontSize * this.lineHeight : entry.y;
+			for (let i = offset; i < offset + entry.string.length; i++) {
+				const charData = this.charData[entry.string.charCodeAt(i - offset)];
+				positions[12 * i] = x + (xOffset + charData.tx1) * fontSize;
+				positions[12 * i + 1] = y + charData.ty1 * fontSize;
+				positions[12 * i + 2] = x + (xOffset + charData.tx2) * fontSize;
+				positions[12 * i + 3] = y + charData.ty1 * fontSize;
+				positions[12 * i + 4] = x + (xOffset + charData.tx2) * fontSize;
+				positions[12 * i + 5] = y + charData.ty2 * fontSize;
+				positions[12 * i + 6] = x + (xOffset + charData.tx1) * fontSize;
+				positions[12 * i + 7] = y + charData.ty1 * fontSize;
+				positions[12 * i + 8] = x + (xOffset + charData.tx1) * fontSize;
+				positions[12 * i + 9] = y + charData.ty2 * fontSize;
+				positions[12 * i + 10] = x + (xOffset + charData.tx2) * fontSize;
+				positions[12 * i + 11] = y + charData.ty2 * fontSize;
+				characters[12 * i] = charData.sx1;
+				characters[12 * i + 1] = charData.sy1;
+				characters[12 * i + 2] = charData.sx2;
+				characters[12 * i + 3] = charData.sy1;
+				characters[12 * i + 4] = charData.sx2;
+				characters[12 * i + 5] = charData.sy2;
+				characters[12 * i + 6] = charData.sx1;
+				characters[12 * i + 7] = charData.sy1;
+				characters[12 * i + 8] = charData.sx1;
+				characters[12 * i + 9] = charData.sy2;
+				characters[12 * i + 10] = charData.sx2;
+				characters[12 * i + 11] = charData.sy2;
 				xOffset += charData.xAdvance;
 			}
-			blur.fill(1 - mapNavigationHandler.zoom * fontSize / parentHeight * 120, offset * 6, (offset + string.length) * 6);
-			offset += string.length;
+			blur.fill(1 - mapNavigationHandler.zoom * fontSize / parentHeight * 120, offset * 6, (offset + entry.string.length) * 6);
+			offset += entry.string.length;
 		}
 
 		this.uniforms.set2f("offset", mapNavigationHandler.x / parentWidth, mapNavigationHandler.y / parentHeight);
@@ -129,4 +126,15 @@ export class GameFont {
 
 		this.context.drawTriangles(2 * characterCount);
 	}
+}
+
+/**
+ * Sizes are in tiles.
+ */
+export type GameTextEntry = {
+	string: string,
+	x: number,
+	y: number,
+	size: number,
+	baselineBottom?: boolean
 }
--- src/renderer/layer/NameRenderer.ts
+++ src/renderer/layer/NameRenderer.ts
@@ -4,7 +4,7 @@ import {gameRenderer, rendererContextGameplay, renderingContextInit} from "../Ga
 import {BaseRendererLayer} from "./BaseRendererLayer";
 import {GameGLContext} from "../GameGLContext";
 import {mapFontData} from "../shader/ShaderManager";
-import {GameFont} from "../GameFont";
+import {GameFont, GameTextEntry} from "../GameFont";
 import {mapNavigationHandler} from "../../game/action/MapNavigationHandler";
 import {formatTroops} from "../../util/StringFormatter";
 
@@ -23,7 +23,7 @@ class NameRenderer extends BaseRendererLayer {
 		const xMax = mapNavigationHandler.getMapX(context.raw.canvas.width);
 		const yMin = mapNavigationHandler.getMapY(0);
 		const yMax = mapNavigationHandler.getMapY(context.raw.canvas.height);
-		const toDraw = [];
+		const toDraw: GameTextEntry[] = [];
 		for (let i = 0; i < playerNameRenderingManager.playerData.length; i++) {
 			const player = playerManager.getPlayer(i);
 			if (player && player.isAlive()) {
@@ -31,8 +31,8 @@ class NameRenderer extends BaseRendererLayer {
 				if (data.size * mapNavigationHandler.zoom < 1 || data.nameX + data.size + 1 < xMin || data.nameX + 1 > xMax || data.nameY + data.size + 1 < yMin || data.nameY + 1 > yMax) {
 					continue;
 				}
-				toDraw.push({string: player.name, nameX: data.nameX, nameY: data.nameY, size: data.size, baselineBottom: true});
-				toDraw.push({string: formatTroops(player.getTroops()), nameX: data.nameX, nameY: data.nameY + data.size / 2, size: data.size});
+				toDraw.push({string: player.name, x: data.nameX, y: data.nameY, size: data.size, baselineBottom: true});
+				toDraw.push({string: formatTroops(player.getTroops()), x: data.nameX, y: data.nameY + data.size / 2, size: data.size});
 			}
 		}
 		this.font.drawText(toDraw);
